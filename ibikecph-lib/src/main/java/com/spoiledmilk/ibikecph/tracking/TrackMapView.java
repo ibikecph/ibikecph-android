400b5105666184b122491edf7d12c2cb3494eabf
Jens Christian Hillerup <jens@hillerup.net>
2015-03-24 14:31:21 +0100

    can show paths on a map now

--- ibikecph-lib/src/main/java/com/spoiledmilk/ibikecph/tracking/TrackMapView.java
+++ ibikecph-lib/src/main/java/com/spoiledmilk/ibikecph/tracking/TrackMapView.java
@@ -1,14 +1,22 @@
 package com.spoiledmilk.ibikecph.tracking;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.overlay.PathOverlay;
+import com.mapbox.mapboxsdk.views.MapView;
 import com.spoiledmilk.ibikecph.R;
+import com.spoiledmilk.ibikecph.persist.Track;
+import com.spoiledmilk.ibikecph.persist.TrackLocation;
+import io.realm.Realm;
+import io.realm.RealmResults;
+
 
 public class TrackMapView extends Activity {
-
+    MapView mapView ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +24,30 @@ public class TrackMapView extends Activity {
 
         int track_position = this.getIntent().getIntExtra("track_position", -1);
 
-        ((TextView) findViewById(R.id.positionText)).setText(track_position+"");
+        mapView = (MapView) findViewById(R.id.mapview);
+
+
+        // Get the route
+        Realm realm = Realm.getInstance(this);
+        RealmResults<Track> tracks = realm.allObjects(Track.class);
+        Track track = tracks.get(track_position);
+
+        PathOverlay path = new PathOverlay(Color.RED, 3);
+        LatLng center = null;
+
+        for (TrackLocation loc : track.getLocations()) {
+            if (center == null) {
+                center = new LatLng(loc.getLatitude(), loc.getLongitude());
+            }
+
+            path.addPoint(loc.getLatitude(), loc.getLongitude());
+        }
+
+        mapView.getOverlays().add(path);
+
+        if (center != null) {
+            mapView.setCenter(center);
+        }
     }
 
 
