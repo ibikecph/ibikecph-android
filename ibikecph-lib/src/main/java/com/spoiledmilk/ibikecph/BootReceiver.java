810c81cbc4ef11a621361de95fd0cb68a6c08ed3
Jens Christian Hillerup <jens@hillerup.net>
2015-05-14 13:54:53 +0200

    Wakes on boot. Needs testing

--- ibikecph-lib/src/main/java/com/spoiledmilk/ibikecph/BootReceiver.java
+++ ibikecph-lib/src/main/java/com/spoiledmilk/ibikecph/BootReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by jens on 5/14/15.
@@ -13,5 +14,8 @@ public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d("JC", "Bootreceiver got boot intent");
+        Toast.makeText(context, "Got boot event", Toast.LENGTH_LONG).show();
+
+        context.startService(new Intent(context, BikeLocationService.class));
     }
 }
