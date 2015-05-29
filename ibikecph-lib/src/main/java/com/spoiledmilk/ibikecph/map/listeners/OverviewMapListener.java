8242ba5421198138cabe9825012d76fff8aa0eb0
Jens Christian Hillerup <jens@hillerup.net>
2015-05-29 14:23:31 +0200

    Erm. That didnt work

--- ibikecph-lib/src/main/java/com/spoiledmilk/ibikecph/map/listeners/OverviewMapListener.java
+++ ibikecph-lib/src/main/java/com/spoiledmilk/ibikecph/map/listeners/OverviewMapListener.java
@@ -1,11 +1,17 @@
 package com.spoiledmilk.ibikecph.map.listeners;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.util.Log;
 import com.mapbox.mapboxsdk.api.ILatLng;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.overlay.Marker;
 import com.mapbox.mapboxsdk.views.MapView;
 import com.mapbox.mapboxsdk.views.MapViewListener;
-import com.spoiledmilk.ibikecph.map.IBCMapView;
+import com.spoiledmilk.ibikecph.IbikeApplication;
+import com.spoiledmilk.ibikecph.R;
 
 /**
  * Created by jens on 5/29/15.
@@ -37,8 +43,24 @@ public class OverviewMapListener implements MapViewListener {
     }
 
     @Override
-    public void onLongPressMap(MapView _mapView, ILatLng iLatLng) {
-        IBCMapView mapView = (IBCMapView) _mapView;
+    public void onLongPressMap(MapView _mapView, ILatLng location) {
+        MapView mapView = _mapView;
+
+        // Geocode the location
+        String title = "Title";
+        String address = "Address";
+
+        // Set a marker
+        Marker m = new Marker(title, address, (LatLng) location);
+        Bitmap bitmap = BitmapFactory.decodeResource(IbikeApplication.getContext().getResources(), R.drawable.location);
+        Bitmap newImage = Bitmap.createBitmap(bitmap, 0, 0, 38, 38);
+        Drawable d = new BitmapDrawable(IbikeApplication.getContext().getResources(), newImage);
+
+        m.setImage(d);
+        mapView.addMarker(m);
+
+        // Invalidate the view so the marker gets drawn.
+        mapView.invalidate();
 
         Log.d("JC", "Long pressed the overview map");
     }
