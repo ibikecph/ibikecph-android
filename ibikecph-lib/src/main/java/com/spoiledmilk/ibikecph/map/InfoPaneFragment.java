c2dab0f3c3505d6ae2d65cb1b991e5b910296a00
Jens Christian Hillerup <jens@hillerup.net>
2015-06-01 21:01:35 +0200

    Added an info pane fragment

---
+++ ibikecph-lib/src/main/java/com/spoiledmilk/ibikecph/map/InfoPaneFragment.java
@@ -0,0 +1,20 @@
+package com.spoiledmilk.ibikecph.map;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.spoiledmilk.ibikecph.R;
+
+/**
+ * Created by jens on 6/1/15.
+ */
+public class InfoPaneFragment extends Fragment {
+
+    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.infopane_empty, container);
+
+        return v;
+    }
+}
