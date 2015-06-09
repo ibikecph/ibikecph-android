63ab524110a53f96e1ed581c8860bcff88eb12a1
Jens Christian Hillerup <jens@hillerup.net>
2015-06-09 14:22:12 +0200

    Changed to staging server, cleaned up the Config

--- ibikecph-lib/src/main/java/com/spoiledmilk/ibikecph/util/Config.java
+++ ibikecph-lib/src/main/java/com/spoiledmilk/ibikecph/util/Config.java
@@ -6,9 +6,8 @@
 package com.spoiledmilk.ibikecph.util;
 
 public class Config {
-	//public static String API_URL = "http://ibikecph-staging.herokuapp.com/api";
-
-	public static String API_URL = "https://www.ibikecph.dk/api";
+	public static String API_URL = "http://ibikecph-staging.herokuapp.com/api";
+	//public static String API_URL = "https://www.ibikecph.dk/api";
 	public static final String API_SERVER_LOGIN = API_URL + "/login";
 	public static final String API_SERVER_REGISTER = API_URL + "/users";
 
@@ -16,11 +15,6 @@ public class Config {
 	public static final String OSRM_SERVER_FAST = "http://routes.ibikecph.dk/v1.1/fast";
 	public static final String OSRM_SERVER_GREEN = "http://routes.ibikecph.dk/v1.1/green";
 	public static final String OSRM_SERVER_CARGO = "http://routes.ibikecph.dk/v1.1/cargo";
-    // old
-//	public static final String OSRM_SERVER_BICYCLE = "http://routes.ibikecph.dk";
-//	public static final String OSRM_SERVER_CARGO = "http://routes.ibikecph.dk/cargo";
-//	public static final String OSRM_SERVER_FAST = "http://routes.ibikecph.dk/fast";
-//	public static final String OSRM_SERVER_GREEN = "http://routes.ibikecph.dk/green";
 	
 	public static boolean GREEN_ROUTES_ENABLED = true;
 
@@ -44,7 +38,6 @@ public class Config {
 	public static final String HOCKEY_APP_ID = "d10e63f39cdbe7e8e3af275207388b2a";
 	public static final boolean HOCKEY_UPDATES_ENABLED = true;
 	public static final boolean ANALYTICS_ENABLED = false;
-    public static final String GOOGLE_API_KEY = "AIzaSyAZwBZgYS-61R-gIvp4GtnekJGGrIKh0Dk";
 	
 	public static boolean EXTENDED_PULL_TOUCH = true;
 	public static boolean TTS_ENABLED = true;
