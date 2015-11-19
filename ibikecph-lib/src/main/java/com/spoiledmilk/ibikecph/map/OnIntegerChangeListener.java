432ec56af2a5c8130bf2db5a5aaea993c8495576
Daniel Velando <dve@it-minds.dk>
2015-11-19 09:41:53 +0100

    Added listener to change the amount of fragment pages

---
+++ ibikecph-lib/src/main/java/com/spoiledmilk/ibikecph/map/OnIntegerChangeListener.java
@@ -0,0 +1,9 @@
+package com.spoiledmilk.ibikecph.map;
+
+/**
+ * Created by Udvikler on 18-11-2015.
+ */
+public interface OnIntegerChangeListener {
+    public void onIntegerChanged(int newValue);
+}
+
