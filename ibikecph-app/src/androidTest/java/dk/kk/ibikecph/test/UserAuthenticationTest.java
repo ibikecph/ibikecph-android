be32463b9b817461a88ee7c8d646f7e557f6a813
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-04-18 21:45:21 +0200

    Enabling instrumented UI testing and implemented the first test

---
+++ ibikecph-app/src/androidTest/java/dk/kk/ibikecph/test/UserAuthenticationTest.java
@@ -0,0 +1,51 @@
+package dk.kk.ibikecph.test;
+
+import com.spoiledmilk.ibikecph.map.MapActivity;
+import com.spoiledmilk.ibikecph.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.support.test.espresso.contrib.DrawerActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.swipeRight;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.action.ViewActions.longClick;
+import static android.support.test.espresso.matcher.ViewMatchers.withChild;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.contrib.DrawerActions.open;
+
+import static org.hamcrest.Matchers.allOf;
+
+/**
+ * Created by kraen on 17-04-16.
+ * The class tests the authentication - logging in and out of the app.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class UserAuthenticationTest {
+
+    @Rule
+    public ActivityTestRule<MapActivity> mActivityRule = new ActivityTestRule<>(MapActivity.class);
+
+
+    @Test
+    public void logout_sameActivity() {
+        // Slide out the drawer menu.
+        onView(withId(R.id.drawer_layout))
+                .perform(open());
+
+        // Click the my-profile item
+        onView(allOf(withParent(withId(R.id.menuListView)), withChild(withText("Min Profil"))))
+                .perform(click());
+
+    }
+}
