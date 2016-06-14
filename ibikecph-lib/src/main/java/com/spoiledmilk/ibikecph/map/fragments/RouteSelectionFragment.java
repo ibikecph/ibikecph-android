d4c44679b452ddf0fd3733ca190113add4acf859
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-06-14 17:40:42 +0200

    Enabled the route selection fragment to handle a null route

--- ibikecph-lib/src/main/java/com/spoiledmilk/ibikecph/map/fragments/RouteSelectionFragment.java
+++ ibikecph-lib/src/main/java/com/spoiledmilk/ibikecph/map/fragments/RouteSelectionFragment.java
@@ -162,119 +162,124 @@ public class RouteSelectionFragment extends MapStateFragment implements View.OnC
     }
 
     public void refreshView() {
-        SMRoute route = mapState.getRoute();
-
-        float distance;
-        float duration;
-        long arrivalTime = 0;
-
-        boolean breakRouteEnabled = getResources().getBoolean(R.bool.breakRouteEnabled);
-        if(breakRouteEnabled) {
-            throw new RuntimeException("Break route needs to be refactored.");
-        }
-        /*
-        // TODO: Move this to the cykelplanen directory.
-        if (IBikeApplication.getAppName().equals("CykelPlanen")) {
-            breakButton = (ImageButton) v.findViewById(R.id.navigationOverviewBreakButton);
-            boolean breakRouteEnabled = getResources().getBoolean(R.bool.breakRouteEnabled);
-            Log.d(getClass().getSimpleName(), "breakRouteEnabled = " + breakRouteEnabled);
-            breakButton.setVisibility(breakRouteEnabled ? View.VISIBLE : View.GONE);
-
-            cargoButton.setVisibility(View.GONE);
-
-            // Set the distance label
-            if (MapActivity.isBreakChosen && Geocoder.totalBikeDistance != null) {
-                distance = Geocoder.totalBikeDistance.get(NavigationMapHandler.obsInt.getPageValue());
-                duration = Geocoder.totalTime.get(NavigationMapHandler.obsInt.getPageValue());
-                arrivalTime = Geocoder.arrivalTime.get(NavigationMapHandler.obsInt.getPageValue());
-                sourceText.setText(IBikeApplication.getString("current_position")); //Just set current position as default because this is the only option working right now.
-                destinationText.setText(DestinationPreviewFragment.name);
-            } else {
-                distance = route.getEstimatedDistance();
-                duration = route.getEstimatedArrivalTime();
-                sourceText.setText(IBikeApplication.getString("current_position")); //Just set current position as default because this is the only option working right now.
-                destinationText.setText(route.endAddress.getStreetAddress());
-            }
-        } else {
-            // Set the distance label
-            distance = route.getEstimatedDistance();
-            duration = route.getEstimatedArrivalTime();
-            sourceText.setText(route.startAddress.getStreetAddress());
-            destinationText.setText(route.endAddress.getStreetAddress());
-        }
-        */
-
         TextView sourceText = (TextView) v.findViewById(R.id.navigationOverviewSource);
         TextView destinationText = (TextView) v.findViewById(R.id.navigationOverviewDestination);
-        sourceText.setText(route.startAddress.getStreetAddress());
-        destinationText.setText(route.endAddress.getStreetAddress());
-
-        distance = route.getEstimatedDistance();
-        duration = route.getEstimatedArrivalTime();
-
         TextView durationText = (TextView) v.findViewById(R.id.navigationOverviewRouteDuration);
         TextView lengthText = (TextView) v.findViewById(R.id.navigationOverviewRouteLength);
         TextView etaText = (TextView) v.findViewById(R.id.navigationOverviewRouteETA);
 
-        if (distance > 1000) {
-            distance /= 1000;
-            lengthText.setText(String.format("%.1f km", distance));
+        SMRoute route = mapState.getRoute();
+        if(route == null) {
+            sourceText.setText("");
+            destinationText.setText("");
         } else {
-            lengthText.setText(String.format("%d m", (int) distance));
-        }
+            float distance;
+            float duration;
+            long arrivalTime = 0;
 
-        // Set the duration label
-        durationText.setText(TrackListAdapter.durationToFormattedTime(duration));
+            boolean breakRouteEnabled = getResources().getBoolean(R.bool.breakRouteEnabled);
+            if(breakRouteEnabled) {
+                throw new RuntimeException("Break route needs to be refactored.");
+            }
+            /*
+            // TODO: Move this to the cykelplanen directory.
+            if (IBikeApplication.getAppName().equals("CykelPlanen")) {
+                breakButton = (ImageButton) v.findViewById(R.id.navigationOverviewBreakButton);
+                boolean breakRouteEnabled = getResources().getBoolean(R.bool.breakRouteEnabled);
+                Log.d(getClass().getSimpleName(), "breakRouteEnabled = " + breakRouteEnabled);
+                breakButton.setVisibility(breakRouteEnabled ? View.VISIBLE : View.GONE);
 
-        SimpleDateFormat sdf = null;
-        if (DateFormat.is24HourFormat(this.getActivity())) {
-            sdf = new SimpleDateFormat("HH:mm");
-        } else {
-            sdf = new SimpleDateFormat("HH:mm a");
-        }
+                cargoButton.setVisibility(View.GONE);
 
-        if (arrivalTime > 0) {
-            // TODO: Move this the the Cykelplanen directory.
-            arrivalTime = arrivalTime * 1000;
-            etaText.setText(sdf.format(arrivalTime).toString());
-        } else {
-            Calendar c = Calendar.getInstance();
-            c.add(Calendar.SECOND, (int) duration);
-            Date arrivalTimee = c.getTime();
-            etaText.setText(sdf.format(arrivalTimee));
-        }
-        // Only show the go button if the route starts at the current location
-        if (route != null && route.startAddress.isCurrentLocation()) {
-            v.findViewById(R.id.startRouteButton).setVisibility(View.VISIBLE);
-        } else if (MapActivity.isBreakChosen) {
-            v.findViewById(R.id.startRouteButton).setVisibility(View.VISIBLE);
-        } else {
-            v.findViewById(R.id.startRouteButton).setVisibility(View.GONE);
+                // Set the distance label
+                if (MapActivity.isBreakChosen && Geocoder.totalBikeDistance != null) {
+                    distance = Geocoder.totalBikeDistance.get(NavigationMapHandler.obsInt.getPageValue());
+                    duration = Geocoder.totalTime.get(NavigationMapHandler.obsInt.getPageValue());
+                    arrivalTime = Geocoder.arrivalTime.get(NavigationMapHandler.obsInt.getPageValue());
+                    sourceText.setText(IBikeApplication.getString("current_position")); //Just set current position as default because this is the only option working right now.
+                    destinationText.setText(DestinationPreviewFragment.name);
+                } else {
+                    distance = route.getEstimatedDistance();
+                    duration = route.getEstimatedArrivalTime();
+                    sourceText.setText(IBikeApplication.getString("current_position")); //Just set current position as default because this is the only option working right now.
+                    destinationText.setText(route.endAddress.getStreetAddress());
+                }
+            } else {
+                // Set the distance label
+                distance = route.getEstimatedDistance();
+                duration = route.getEstimatedArrivalTime();
+                sourceText.setText(route.startAddress.getStreetAddress());
+                destinationText.setText(route.endAddress.getStreetAddress());
+            }
+            */
+
+            sourceText.setText(route.startAddress.getStreetAddress());
+            destinationText.setText(route.endAddress.getStreetAddress());
+
+            distance = route.getEstimatedDistance();
+            duration = route.getEstimatedArrivalTime();
+
+            if (distance > 1000) {
+                distance /= 1000;
+                lengthText.setText(String.format("%.1f km", distance));
+            } else {
+                lengthText.setText(String.format("%d m", (int) distance));
+            }
+
+            // Set the duration label
+            durationText.setText(TrackListAdapter.durationToFormattedTime(duration));
+
+            SimpleDateFormat sdf = null;
+            if (DateFormat.is24HourFormat(this.getActivity())) {
+                sdf = new SimpleDateFormat("HH:mm");
+            } else {
+                sdf = new SimpleDateFormat("HH:mm a");
+            }
+
+            if (arrivalTime > 0) {
+                // TODO: Move this the the Cykelplanen directory.
+                arrivalTime = arrivalTime * 1000;
+                etaText.setText(sdf.format(arrivalTime).toString());
+            } else {
+                Calendar c = Calendar.getInstance();
+                c.add(Calendar.SECOND, (int) duration);
+                Date arrivalTimee = c.getTime();
+                etaText.setText(sdf.format(arrivalTimee));
+            }
+            // Only show the go button if the route starts at the current location
+            if (route != null && route.startAddress.isCurrentLocation()) {
+                v.findViewById(R.id.startRouteButton).setVisibility(View.VISIBLE);
+            } else if (MapActivity.isBreakChosen) {
+                v.findViewById(R.id.startRouteButton).setVisibility(View.VISIBLE);
+            } else {
+                v.findViewById(R.id.startRouteButton).setVisibility(View.GONE);
+            }
         }
 
         // Make sure the buttons are in the disabled state if not enabled.
-
         int greenButtonDrawable = R.drawable.btn_route_green_disabled;
         int cargoButtonDrawable = R.drawable.btn_route_cargo_disabled;
         int fastButtonDrawable = R.drawable.btn_route_fastest_disabled;
         int breakButtonDrawable = R.drawable.btn_train_disabled;
 
-        // Highlight the relevant route type button
-        switch (route.getType()) {
-            case FASTEST:
-                fastButtonDrawable = R.drawable.btn_route_fastest_enabled;
-                break;
-            case CARGO:
-                cargoButtonDrawable = R.drawable.btn_route_cargo_enabled;
-                break;
-            case GREEN:
-                greenButtonDrawable = R.drawable.btn_route_green_enabled;
-                break;
-            case BREAK:
-                breakButtonDrawable = R.drawable.btn_train_enabled;
-                break;
-            default:
-                break;
+        if(route != null) {
+            // Highlight the relevant route type button
+            switch (route.getType()) {
+                case FASTEST:
+                    fastButtonDrawable = R.drawable.btn_route_fastest_enabled;
+                    break;
+                case CARGO:
+                    cargoButtonDrawable = R.drawable.btn_route_cargo_enabled;
+                    break;
+                case GREEN:
+                    greenButtonDrawable = R.drawable.btn_route_green_enabled;
+                    break;
+                case BREAK:
+                    breakButtonDrawable = R.drawable.btn_train_enabled;
+                    break;
+                default:
+                    break;
+            }
         }
 
         greenButton.setImageResource(greenButtonDrawable);
