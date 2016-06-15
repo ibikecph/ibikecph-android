f05ba4c8e17896150cb9def72e13af0939b8604e
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-06-15 19:29:40 +0200

    OSRM v5 returns status 200 on success

--- ibikecph-lib/src/main/java/com/spoiledmilk/ibikecph/navigation/routing_engine/SMRoute.java
+++ ibikecph-lib/src/main/java/com/spoiledmilk/ibikecph/navigation/routing_engine/SMRoute.java
@@ -178,7 +178,7 @@ public class SMRoute implements SMHttpRequestListener, LocationListener {
             case SMHttpRequest.REQUEST_GET_RECALCULATED_ROUTE:
                 final JsonNode jRoot = ((RouteInfo) response).jsonRoot;
 
-                if (jRoot == null || jRoot.path("status").asInt() != 0) {
+                if (jRoot == null || jRoot.path("status").asInt() != 200) {
                     if (listener != null) {
                         listener.serverError();
                     }
