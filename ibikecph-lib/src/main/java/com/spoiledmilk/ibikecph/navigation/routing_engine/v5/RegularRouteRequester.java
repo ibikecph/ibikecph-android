b1dd6eac5a4b927b51ad3229a84f18ce51846ebe
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-08-12 10:08:15 +0200

    Refactored out the non-v5 RegularRouteRequester

--- ibikecph-lib/src/main/java/com/spoiledmilk/ibikecph/navigation/routing_engine/v5/RegularRouteRequester.java
+++ ibikecph-lib/src/main/java/com/spoiledmilk/ibikecph/navigation/routing_engine/v5/RegularRouteRequester.java
@@ -6,10 +6,12 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.mapbox.mapboxsdk.api.ILatLng;
 import com.spoiledmilk.ibikecph.map.Geocoder;
 import com.spoiledmilk.ibikecph.map.RouteType;
+import com.spoiledmilk.ibikecph.navigation.routing_engine.RouteRequester;
 import com.spoiledmilk.ibikecph.navigation.routing_engine.SMRoute;
 import com.spoiledmilk.ibikecph.util.Config;
 import com.spoiledmilk.ibikecph.util.Util;
 
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Locale;
@@ -17,13 +19,45 @@ import java.util.Locale;
 /**
  * Created by kraen on 11-07-16.
  */
-public class RegularRouteRequester extends com.spoiledmilk.ibikecph.navigation.routing_engine.RegularRouteRequester {
+public class RegularRouteRequester extends RouteRequester {
 
     private Float bearing = null;
-    protected String destinationHint;
+    protected RouteType type;
+    protected SMRoute route;
+
+    public void setRoute(SMRoute route) {
+        this.route = route;
+    }
 
     public RegularRouteRequester(ILatLng start, ILatLng end, Geocoder.RouteCallback callback, RouteType type) {
-        super(start, end, callback, type);
+        super(start, end, callback);
+        this.type = type;
+    }
+
+    @Override
+    protected Boolean doInBackground(Void... params) {
+        HttpURLConnection connection = null;
+        try {
+            URL url = generateURL();
+            Log.d("RegularRouteRequester", "Requesting " + url.toString());
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Accept", "application/vnd.ibikecph.v1");
+
+            if (connection.getResponseCode() != 200) {
+                throw new RuntimeException("Unexpected status code: " + connection.getResponseCode());
+            }
+            JsonNode node = Util.getJsonObjectMapper().readValue(connection.getInputStream(), JsonNode.class);
+            parseJSON(node);
+            return true;
+        } catch (Exception e) {
+            Log.e("RegularRouteRequester", "Error requesting route", e);
+            return false;
+        } finally {
+            if(connection != null) {
+                connection.disconnect();
+            }
+        }
     }
 
     protected URL generateURL() {
@@ -69,7 +103,6 @@ public class RegularRouteRequester extends com.spoiledmilk.ibikecph.navigation.r
         }
     }
 
-    @Override
     protected SMRoute parseJSON(JsonNode node) {
         if (route == null) {
             route = new Route(Util.locationFromGeoPoint(start), Util.locationFromGeoPoint(end), type);
@@ -81,8 +114,11 @@ public class RegularRouteRequester extends com.spoiledmilk.ibikecph.navigation.r
     @Override
     protected void onPostExecute(Boolean success) {
         super.onPostExecute(success);
-        if (route != null && success) {
+        if(success && route != null) {
+            callback.onSuccess(route);
             route.emitRouteUpdated();
+        } else {
+            callback.onFailure();
         }
     }
 
